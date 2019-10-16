03bbbbcc751e3a8d91dc259a0b35a90a6526ef3f
dambar <notdambar@gmail.com>
2019-10-16 21:36:57 +0800

    data trasnfer converter added

---
+++ js/data_trasfer_rate.js
@@ -0,0 +1,642 @@
+function bitToGibibit(bit) {
+    return bit / 1073742000;
+}
+
+function bitToGigabit(bit) {
+    return bit / 1000000000;
+}
+
+function bitToGigabyte() {
+    return bit / 8000000000;
+}
+
+function bitToKibibit(bit) {
+    return bit / 1024;
+}
+
+function bitToKilobit(bit) {
+    return bit / 1000;
+}
+
+function bitToKilobyte(bit) {
+    return bit / 8000;
+}
+
+function bitToMebibit(bit) {
+    return bit / 1048576;
+}
+
+function bitToMegabit(bit) {
+    return bit / 1000000;
+}
+
+function bitToMegabyte(bit) {
+    return bit / 8000000;
+}
+
+function bitToTebibit(bit) {
+    return bit / 1099512000000;
+}
+
+function bitToTerabit(bit) {
+    return bit / 1000000000000;
+}
+
+function bitToTerabyte(bit) {
+    return bit / 8000000000000;
+}
+
+///////////////////////////////
+
+function gibibitToBit(gibibit) {
+    return gibibit * 1073742000;
+}
+
+function gibibitToGigabit(gibibit) {
+    return bitToGibibit(gibibitToBit(gibibit));
+}
+
+function gibibitToGigabyte(gibibit) {
+    return bitToGigabyte(gibibitToBit(gibibit));
+}
+
+function gibibitToKibibit(gibibit) {
+    return bitToKibibit(gibibitToBit(gibibit));
+}
+
+function gibibitToKilobit(gibibit) {
+    return bitToKilobit(gibibitToBit(gibibit));
+}
+
+function gibibitToKilobyte(gibibit) {
+    return bitToKilobyte(gibibitToBit(gibibit));
+}
+
+function gibibitToMebibit(gibibit) {
+    return bitToMebibit(gibibitToBit(gibibit));
+}
+
+function gibibitToMegabit(gibibit) {
+    return bitToMegabit(gibibitToBit(gibibit));
+}
+
+function gibibitToMegabyte(gibibit) {
+    return bitToMegabyte(gibibitToBit(gibibit));
+}
+
+function gibibitToTebibit(gibibit) {
+    return bitToTebibit(gibibitToBit(gibibit));
+}
+
+function gibibitToTerabit(gibibit) {
+    return bitToTerabit(gibibitToBit(gibibit));
+}
+
+function gibibitToTerabyte(gibibit) {
+    return bitToTerabyte(gibibitToBit(gibibit));
+}
+
+/////////////////////////////////////////////////
+
+function gigabitToBit(gigabit) {
+    return gigabit * 1000000000;
+}
+
+function gigabitToGibibit(gigabit) {
+    return bitToGibibit(gigabitToBit(gigabit));
+}
+
+function gigabitToGigabyte(gigabit) {
+    return bitToGigabyte(gigabitToBit(gigabit));
+}
+
+function gigabitToKibibit(gigabit) {
+    return bitToKibibit(gigabitToBit(gigabit));
+}
+
+function gigabitToKilobit(gigabit) {
+    return bitToKilobit(gigabitToBit(gigabit));
+}
+
+function gigabitToKilobyte(gigabit) {
+    return bitToKilobyte(gigabitToBit(gigabit));
+}
+
+function gigabitToMebibit(gigabit) {
+    return bitToMebibit(gigabitToBit(gigabit));
+}
+
+function gigabitToMegabit(gigabit) {
+    return bitToMegabit(gigabitToBit(gigabit));
+}
+
+function gigabitToMegabyte(gigabit) {
+    return bitToMegabyte(gigabitToBit(gigabit));
+}
+
+function gigabitToTebibit(gigabit) {
+    return bitToTebibit(gigabitToBit(gigabit));
+}
+
+function gigabitToTerabit(gigabit) {
+    return bitToTerabit(gigabitToBit(gigabit));
+}
+
+function gigabitToTerabyte(gigabit) {
+    return bitToTerabyte(gigabitToBit(gigabit));
+}
+
+////////////////////////////////////////////////
+
+function gigabyteToBit(gigabyte) {
+    return gigabyte * 8000000000;
+}
+
+function gigabyteToGibibit(gigabyte) {
+    return bitToGibibit(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToGigabit(gigabyte) {
+    return bitToGigabit(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToKibibit(gigabyte) {
+    return bitToKibibit(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToKilobit(gigabyte) {
+    return bitToKilobit(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToKilobyte(gigabyte) {
+    return bitToKilobyte(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToMebibit(gigabyte) {
+    return bitToMebibit(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToMegabit(gigabyte) {
+    return bitToMegabit(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToMegabyte(gigabyte) {
+    return bitToMegabyte(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToTebibit(gigabyte) {
+    return bitToTebibit(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToTerabit(gigabyte) {
+    return bitToTerabit(gigabyteToBit(gigabyte));
+}
+
+function gigabyteToTerabyte(gigabyte) {
+    return bitToTerabyte(gigabyteToBit(gigabyte));
+}
+///////////////////////////////////////////////////
+function kibibitToBit(kibibit) {
+    return kibibit * 1024;
+}
+
+function kibibitToGibibit(kibibit) {
+    return bitToGibibit(kibibitToBit(kibibit));
+}
+
+function kibibitToGigabit(kibibit) {
+    return bitToGigabit(kibibitToBit(kibibit));
+}
+
+function kibibitToGigabyte(kibibit) {
+    return bitToGigabyte(kibibitToBit(kibibit));
+}
+
+function kibibitToKilobit(kibibit) {
+    return bitToKilobit(kibibitToBit(kibibit));
+}
+
+function kibibitToKilobyte(kibibit) {
+    return bitToKilobyte(kibibitToBit(kibibit));
+}
+
+function kibibitToMebibit(kibibit) {
+    return bitToMebibit(kibibitToBit(kibibit));
+}
+
+function kibibitToMegabit(kibibit) {
+    return bitToMegabit(kibibitToBit(kibibit));
+}
+
+function kibibitToMegabyte(kibibit) {
+    return bitToMegabyte(kibibitToBit(kibibit));
+}
+
+function kibibitToTebibit(kibibit) {
+    return bitToTebibit(kibibitToBit(kibibit));
+}
+
+function kibibitToTerabit(kibibit) {
+    return bitToTerabit(kibibitToBit(kibibit));
+}
+
+function kibibitToTerabyte(kibibit) {
+    return bitToTerabyte(kibibitToBit(kibibit));
+}
+
+//////////////////////////////////////////////
+
+function kilobitToBit(kilobit) {
+    return kilobit * 1000;
+}
+
+function kilobitToGibibit(kilobit) {
+    return bitToGibibit(kilobitToBit(kilobit));
+}
+
+function kilobitToGigabit(kilobit) {
+    return bitToGigabit(kilobitToBit(kilobit));
+}
+
+function kilobitToGigabyte(kilobit) {
+    return bitToGigabyte(kilobitToBit(kilobit));
+}
+
+function kilobitToKibibit(kilobit) {
+    return bitToKibibit(kilobitToBit(kilobit));
+}
+
+function kilobitToKilobyte(kilobit) {
+    return bitToKilobyte(kilobitToBit(kilobit));
+}
+
+function kilobitToMebibit(kilobit) {
+    return bitToMebibit(kilobitToBit(kilobit));
+}
+
+function kilobitToMegabit(kilobit) {
+    return bitToMegabit(kilobitToBit(kilobit));
+}
+
+function kilobitToMegabyte(kilobit) {
+    return bitToMegabyte(kilobitToBit(kilobit));
+}
+
+function kilobitToTebibit(kilobit) {
+    return bitToTebibit(kilobitToBit(kilobit));
+}
+
+function kilobitToTerabit(kilobit) {
+    return bitToTerabit(kilobitToBit(kilobit));
+}
+
+function kilobitToTerabyte(kilobit) {
+    return bitToTerabyte(kilobitToBit(kilobit));
+}
+
+/////////////////////////////////////////////
+
+function kilobyteToBit(kilobyte) {
+    return kilobyte * 8000;
+}
+
+function kilobyteToGibibit(kilobyte) {
+    return bitToGibibit(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToGigabit(kilobyte) {
+    return bitToGigabit(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToGigabyte(kilobyte) {
+    return bitToGigabyte(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToKibibit(kilobyte) {
+    return bitToKibibit(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToKilobit(kilobyte) {
+    return bitToKilobit(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToMebibit(kilobyte) {
+    return bitToMebibit(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToMegabit(kilobyte) {
+    return bitToMegabit(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToMegabyte(kilobyte) {
+    return bitToMegabyte(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToTebibit(kilobyte) {
+    return bitToTebibit(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToTerabit(kilobyte) {
+    return bitToTerabit(kilobyteToBit(kilobyte));
+}
+
+function kilobyteToTerabyte(kilobyte) {
+    return bitToTerabyte(kilobyteToBit(kilobyte));
+}
+
+////////////////////////////////////////////////
+
+function mebibitToBit(mebibit) {
+    return mebibit * 1048576;
+}
+
+function mebibitToGibibit(mebibit) {
+    return bitToGibibit(mebibitToBit(mebibit));
+}
+
+function mebibitToGigabit(mebibit) {
+    return bitToGigabit(mebibitToBit(mebibit));
+}
+
+function mebibitToGigabyte(mebibit) {
+    return bitToGigabyte(mebibitToBit(mebibit));
+}
+
+function mebibitToKibibit(mebibit) {
+    return bitToKibibit(mebibitToBit(mebibit));
+}
+
+function mebibitToKilobit(mebibit) {
+    return bitToKilobit(mebibitToBit(mebibit));
+}
+
+function mebibitToKilobyte(mebibit) {
+    return bitToKilobyte(mebibitToBit(mebibit));
+}
+
+function mebibitToMegabit(mebibit) {
+    return bitToMegabit(mebibitToBit(mebibit));
+}
+
+function mebibitToMegabyte(mebibit) {
+    return bitToMegabyte(mebibitToBit(mebibit));
+}
+
+function mebibitToTebibit(mebibit) {
+    return bitToTebibit(mebibitToBit(mebibit));
+}
+
+function mebibitToTerabit(mebibit) {
+    return bitToTerabit(mebibitToBit(mebibit));
+}
+
+function mebibitToTerabyte(mebibit) {
+    return bitToTerabyte(mebibitToBit(mebibit));
+}
+
+//////////////////////////////////////////////////
+
+function megabitToBit(megabit) {
+    return megabit * 1000000;
+}
+
+function megabitToGibibit(megabit) {
+    return bitToGibibit(megabitToBit(megabit));
+}
+
+function megabitToGigabit(megabit) {
+    return bitToGigabit(megabitToBit(megabit));
+}
+
+function megabitToGigabyte(megabit) {
+    return bitToGigabyte(megabitToBit(megabit));
+}
+
+function megabitToKibibit(megabit) {
+    return bitToKibibit(megabitToBit(megabit));
+}
+
+function megabitToKilobit(megabit) {
+    return bitToKilobit(megabitToBit(megabit));
+}
+
+function megabitToKilobyte(megabit) {
+    return bitToKilobyte(megabitToBit(megabit));
+}
+
+function megabitToMebibit(megabit) {
+    return bitToMebibit(megabitToBit(megabit));
+}
+
+function megabitToMegabyte(megabit) {
+    return bitToMegabyte(megabitToBit(megabit));
+}
+
+function megabitToTebibit(megabit) {
+    return bitToTebibit(megabitToBit(megabit));
+}
+
+function megabitToTerabit(megabit) {
+    return bitToTerabit(megabitToBit(megabit));
+}
+
+function megabitToTerabyte(megabit) {
+    return bitToTerabyte(megabitToBit(megabit));
+}
+////////////////////////////////////////////////////
+
+function megabyteToBit(megabyte) {
+    return megabyte * 8000000;
+}
+
+function megabyteToGibibit(megabyte) {
+    return bitToGibibit(megabyteToBit(megabyte));
+}
+
+function megabyteToGigabit(megabyte) {
+    return bitToGigabit(megabyteToBit(megabyte));
+}
+
+function megabyteToGigabyte(megabyte) {
+    return bitToGigabyte(megabyteToBit(megabyte));
+}
+
+function megabyteToKibibit(megabyte) {
+    return bitToKibibit(megabyteToBit(megabyte));
+}
+
+function megabyteToKilobit(megabyte) {
+    return bitToKilobit(megabyteToBit(megabyte));
+}
+
+function megabyteToKilobyte(megabyte) {
+    return bitToKilobyte(megabyteToBit(megabyte));
+}
+
+function megabyteToMebibit(megabyte) {
+    return bitToMebibit(megabyteToBit(megabyte));
+}
+
+function megabyteToMegabit(megabyte) {
+    return bitToMegabit(megabyteToBit(megabyte));
+}
+
+function megabyteToTebibit(megabyte) {
+    return bitToTebibit(megabyteToBit(megabyte));
+}
+
+function megabyteToTerabit(megabyte) {
+    return bitToTerabit(megabyteToBit(megabyte));
+}
+
+function megabyteToTerabyte(megabyte) {
+    return bitToTerabyte(megabyteToBit(megabyte));
+}
+//////////////////////////////////////////////
+
+function tebibitToBit(tebibit) {
+    return tebibit * 1099512000000;
+}
+
+function tebibitToGibibit(tebibit) {
+    return bitToGibibit(tebibitToBit(tebibit));
+}
+
+function tebibitToGigabit(tebibit) {
+    return bitToGigabit(tebibitToBit(tebibit));
+}
+
+function tebibitToGigabyte(tebibit) {
+    return bitToGigabyte(tebibitToBit(tebibit));
+}
+
+function tebibitToKibibit(tebibit) {
+    return bitToKibibit(tebibitToBit(tebibit));
+}
+
+function tebibitToKilobit(tebibit) {
+    return bitToKilobit(tebibitToBit(tebibit));
+}
+
+function tebibitToKilobyte(tebibit) {
+    return bitToKilobyte(tebibitToBit(tebibit));
+}
+
+function tebibitToMebibit(tebibit) {
+    return bitToMebibit(tebibitToBit(tebibit));
+}
+
+function tebibitToMegabit(tebibit) {
+    return bitToMegabit(tebibitToBit(tebibit));
+}
+
+function tebibitToMegabyte(tebibit) {
+    return bitToMegabyte(tebibitToBit(tebibit));
+}
+
+function tebibitToTerabit(tebibit) {
+    return bitToTerabit(tebibitToBit(tebibit));
+}
+
+function tebibitToTerabyte(tebibit) {
+    return bitToTerabyte(tebibitToBit(tebibit));
+}
+
+//////////////////////////////////////////////////
+
+function terabitToBit(terabit) {
+    return terabit * 1000000000000;
+}
+
+function terabitToGibibit(terabit) {
+    return bitToGibibit(terabitToBit(terabit));
+}
+
+function terabitToGigabit(terabit) {
+    return bitToGigabit(terabitToBit(terabit));
+}
+
+function terabitToGigabyte(terabit) {
+    return bitToGigabyte(terabitToBit(terabit));
+}
+
+function terabitToKibibit(terabit) {
+    return bitToKibibit(terabitToBit(terabit));
+}
+
+function terabitToKilobit(terabit) {
+    return bitToKilobit(terabitToBit(terabit));
+}
+
+function terabitToKilobyte(terabit) {
+    return bitToKilobyte(terabitToBit(terabit));
+}
+
+function terabitToMebibit(terabit) {
+    return bitToMebibit(terabitToBit(terabit));
+}
+
+function terabitToMegabit(terabit) {
+    return bitToMegabit(terabitToBit(terabit));
+}
+
+function terabitToMegabyte(terabit) {
+    return bitToMegabyte(terabitToBit(terabit));
+}
+
+function terabitToTebibit(terabit) {
+    return bitToTebibit(terabitToBit(terabit));
+}
+
+function terabitToTerabyte(terabit) {
+    return bitToTerabyte(terabitToBit(terabit));
+}
+//////////////////////////////////////////////
+
+function terabyteToBit(terabyte) {
+    return terabyte * 8000000000000;
+}
+
+function terabyteToGibibit(terabyte) {
+    return bitToGibibit(terabyteToBit(terabyte));
+}
+
+function terabyteToGigabit(terabyte) {
+    return bitToGigabit(terabyteToBit(terabyte));
+}
+
+function terabyteToGigabyte(terabyte) {
+    return bitToGigabyte(terabyteToBit(terabyte));
+}
+
+function terabyteToKibibit(terabyte) {
+    return bitToKibibit(terabyteToBit(terabyte));
+}
+
+function terabyteToKilobit(terabyte) {
+    return bitToKilobit(terabyteToBit(terabyte));
+}
+
+function terabyteToKilobyte(terabyte) {
+    return bitToKilobyte(terabyteToBit(terabyte));
+}
+
+function terabyteToMebibit(terabyte) {
+    return bitToMebibit(terabyteToBit(terabyte));
+}
+
+function terabyteToMegabit(terabyte) {
+    return bitToMegabit(terabyteToBit(terabyte));
+}
+
+function terabyteToMegabyte(terabyte) {
+    return bitToMegabyte(terabyteToBit(terabyte));
+}
+
+function terabyteToTebibit(terabyte) {
+    return bitToTebibit(terabyteToBit(terabyte));
+}
+
+function terabyteToTerabit(terabyte) {
+    return bitToTerabit(terabyteToBit(terabyte));
+}
